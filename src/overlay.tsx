7719b1c30a2ea0dbd4e94705d36eeceec03b8977
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-03-09 12:49:38 -0800

    Created overlay

---
+++ src/overlay.tsx
@@ -0,0 +1,7 @@
+import { render } from "solid-js/web";
+import "./styles.scss";
+
+render(
+  () => <div class="mt-4 text-center text-4xl">hello</div>,
+  document.getElementById("root") as HTMLElement
+);
