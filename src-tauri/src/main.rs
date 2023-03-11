c78c68dfc9298804e025ec01b54a0df9dd9f3f07
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2023-03-11 13:54:30 -0800

    Upscaled and grayed out screenshot

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use image::{imageops, open};
 use screenshots::Screen;
 use std::fs;
 
@@ -11,6 +12,12 @@ fn screenshot(x: i32, y: i32, width: u32, height: u32) {
     let image = screen.capture_area(x, y, width, height).unwrap();
     let buffer = image.buffer();
     fs::write("target/screenshot.png", buffer).unwrap();
+    open("target/screenshot.png")
+        .unwrap()
+        .resize(width * 2, height * 2, imageops::Lanczos3)
+        .grayscale()
+        .save("target/upscale_grayscale.png")
+        .unwrap()
 }
 
 fn main() {
